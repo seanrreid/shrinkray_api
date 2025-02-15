266c9ac5cb29e827cda1f63c3e50a220a27fc2f7
Sean R Reid <sean@torchcodelab.com>
2025-02-15 15:18:57 -0500

    Put POST/PUT/DELETE routes behind login

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UrlController;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('/urls', [UrlController::class, 'index']);
 Route::get('/urls/{shortCode}', [UrlController::class, 'single_url']);
-Route::post('/urls', [UrlController::class, 'add']);
-Route::put('/urls/{id}', [UrlController::class, 'update']);
-Route::delete('/urls/{id}', [UrlController::class, 'delete']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/urls', [UrlController::class, 'add']);
+    Route::put('/urls/{id}', [UrlController::class, 'update']);
+    Route::delete('/urls/{id}', [UrlController::class, 'delete']);
+});
